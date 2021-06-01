be12d74f5eb3058aff18deb94ced8c153be13ea6
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-06-01 15:28:31 -0300

    :tada: create flutter project

---
+++ android/app/src/main/kotlin/com/example/gamepass_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gamepass_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:gamepass_clone/UI/app.dart';
+
+void main() {
+  runApp(App());
+}
