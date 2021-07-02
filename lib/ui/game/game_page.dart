67a236a070dbc9d2e25332bac9f2c771e39e0d12
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-07-02 18:48:06 -0300

    :sparkles: add trailer controls

--- lib/ui/game/game_page.dart
+++ lib/ui/game/game_page.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'package:gamepass_clone/domain/game.dart';
 import 'package:gamepass_clone/ui/shared/utils/get_youtube_id.dart';
 import 'package:gamepass_clone/ui/theme/colors.dart';
 import 'package:gamepass_clone/ui/theme/fonts.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class GamePage extends StatefulWidget {
   final Game game;
 
-  const GamePage({
-    Key? key,
-    required this.game,
-  }) : super(key: key);
+  const GamePage({Key? key, required this.game}) : super(key: key);
 
   @override
   _GamePageState createState() => _GamePageState();
@@ -20,7 +17,7 @@ class GamePage extends StatefulWidget {
 
 class _GamePageState extends State<GamePage> {
   late YoutubePlayerController trailerController;
-  bool isMuted = false, isPaused = false;
+  bool isMuted = false, isPaused = false, isEnded = false, isStarted = false;
 
   @override
   void didChangeDependencies() {
@@ -50,44 +47,44 @@ class _GamePageState extends State<GamePage> {
           padding: EdgeInsets.only(bottom: 16),
           children: [
             Container(
+              height: (MediaQuery.of(context).size.height ~/ 1.8).toDouble(),
+              width: double.maxFinite,
+              child: Container(
                 height: (MediaQuery.of(context).size.height ~/ 1.8).toDouble(),
                 width: double.maxFinite,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                    image: NetworkImage(widget.game.logo),
+                  ),
+                ),
                 child: Container(
-                  height:
-                      (MediaQuery.of(context).size.height ~/ 1.8).toDouble(),
-                  width: double.maxFinite,
                   decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      alignment: Alignment.topCenter,
-                      image: NetworkImage(widget.game.logo),
+                    gradient: LinearGradient(
+                      colors: [Colors.transparent, ThemeColors.background],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
                     ),
                   ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.transparent, ThemeColors.background],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                      ),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 28, horizontal: 14),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(40),
-                            ),
-                            color: Colors.black26,
+                  padding: EdgeInsets.symmetric(vertical: 28, horizontal: 14),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(40),
                           ),
-                          child: BackButton(color: Colors.white),
+                          color: Colors.black26,
                         ),
-                      ],
-                    ),
+                        child: BackButton(color: Colors.white),
+                      ),
+                    ],
                   ),
-                )),
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.only(top: 4.0, left: 14, right: 14),
               child: Column(
@@ -116,7 +113,7 @@ class _GamePageState extends State<GamePage> {
                     child: _buildYoutubePlayer(),
                     title: "Trailer",
                   ),
-                  SizedBox(height: 16),
+                  SizedBox(height: 8),
                   _buildContent(
                     child: Text(
                       widget.game.size,
@@ -146,20 +143,44 @@ class _GamePageState extends State<GamePage> {
       child: Stack(
         alignment: Alignment.bottomRight,
         children: [
-          YoutubePlayer(
-            controller: trailerController,
-          ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: YoutubePlayer(
+              controller: trailerController,
+              onEnded: (_) {
+                setState(() {
+                  isEnded = true;
+                });
+              },
+              onReady: () {
+                setState(() {
+                  isStarted = true;
+                });
+              },
+            ),
+          ),
+          _buildPlayerControls()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlayerControls() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: isStarted
+          ? Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(
                   decoration: BoxDecoration(
-                    color: Colors.black87,
+                    color: Colors.black,
                     borderRadius: BorderRadius.all(Radius.circular(44)),
                   ),
+                  width: 40,
+                  height: 40,
                   child: IconButton(
+                    iconSize: 24,
                     onPressed: () {
                       if (isMuted)
                         trailerController.unMute();
@@ -179,33 +200,57 @@ class _GamePageState extends State<GamePage> {
                   ),
                 ),
                 SizedBox(width: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black87,
-                    borderRadius: BorderRadius.all(Radius.circular(44)),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      if (!isPaused)
-                        trailerController.pause();
-                      else
-                        trailerController.play();
+                !isEnded
+                    ? Container(
+                        decoration: BoxDecoration(
+                          color: ThemeColors.primary,
+                          borderRadius: BorderRadius.all(Radius.circular(44)),
+                        ),
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                          iconSize: 24,
+                          onPressed: () {
+                            if (!isPaused)
+                              trailerController.pause();
+                            else
+                              trailerController.play();
 
-                      setState(() {
-                        isPaused = !isPaused;
-                      });
-                    },
-                    icon: Icon(
-                      isPaused ? Icons.play_arrow : Icons.pause,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
+                            setState(() {
+                              isPaused = !isPaused;
+                            });
+                          },
+                          icon: Icon(
+                            isPaused ? Icons.play_arrow : Icons.pause,
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
+                          color: ThemeColors.primary,
+                          borderRadius: BorderRadius.all(Radius.circular(44)),
+                        ),
+                        width: 40,
+                        height: 40,
+                        child: IconButton(
+                          iconSize: 24,
+                          onPressed: () {
+                            trailerController.play();
+
+                            setState(() {
+                              isEnded = false;
+                            });
+                          },
+                          icon: Icon(
+                            Icons.replay,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
               ],
-            ),
-          )
-        ],
-      ),
+            )
+          : null,
     );
   }
 
